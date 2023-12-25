e5fde3d29aa4ac406128b9fac661495866633a3a
mateus-gomes-barros <mateusgomesbarros@hotmail.com>
2023-12-25 18:18:16 -0300

    fix:avatar

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <header className="flex flex-row items-center gap-2 w-full max-w-full bg-gradient-to-r from-[#521818] from-40%  via-[#df4d4d] via-100% to-100% to-[#f7cabc] px-8 py-4">
       <Image
-        src="/images/avatar.jpg"
+        src="/images/avatar.2.png"
         alt="avatar.2"
         className="h-10 w-10 rounded-[50rem] border-[1px] border-gray-50"
         width={40}
