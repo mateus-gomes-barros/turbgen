36be2c1b95ec6b5087fd8762c0ce863dd7ba3085
mateus-gomes-barros <mateusgomesbarros@hotmail.com>
2023-12-25 18:15:47 -0300

    avatar:layout

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -7,14 +7,14 @@ export default function Header() {
     <header className="flex flex-row items-center gap-2 w-full max-w-full bg-gradient-to-r from-[#521818] from-40%  via-[#df4d4d] via-100% to-100% to-[#f7cabc] px-8 py-4">
       <Image
         src="/images/avatar.jpg"
-        alt="avatar"
+        alt="avatar.2"
         className="h-10 w-10 rounded-[50rem] border-[1px] border-gray-50"
         width={40}
         height={40}
       />
       {/* <CircleUserRound color='white' size={40}/> */}
       <div>
-        <p className="text-white">Vinicius</p>
+        <p className="text-white">TurbGen</p>
       </div>
     </header>
   );
