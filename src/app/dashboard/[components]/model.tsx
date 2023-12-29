c03c9ec023b015255450d752030809e35d1ec55a
mateus-gomes-barros <mateusgomesbarros@hotmail.com>
2023-12-29 11:05:52 -0300

    chore: styles model

--- src/app/dashboard/[components]/model.tsx
+++ src/app/dashboard/[components]/model.tsx
@@ -8,14 +8,14 @@ type Props = {
 export default function Model({ close, children }: Props) {
   return (
     <div className="w-screen h-screen bg-[#000000b7] flex justify-center items-center fixed left-0 top-0">
-      <div className="w-[60%] h-[80%] bg-[#000000] rounded-2xl relative flex items-center justify-center animate-fadeIn">
+      <div className="w-[70%] h-[80%] bg-[#000000] rounded-2xl relative flex items-center justify-center animate-fadeIn">
         <XCircle
           color={"#ffffff"}
           className="absolute right-[16px] top-[16px] cursor-pointer"
           size={30}
           onClick={close}
         />
-        <div className="w-[95%] h-[85%] relative">
+        <div className="w-[80%] h-[90%] relative">
           {children}
         </div>
       </div>
